303a53c2ebcb7470dff0c5f242f1a19f77cd12af
Adam Mwaniki <adamndegwa@protonmail.com>
2024-08-06 23:59:52 +0300

    Adds migrations

--- server/config.py
+++ server/config.py
@@ -9,17 +9,19 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+import os
 
 # Local imports
 
 # Instantiate app, set attributes
 app = Flask(__name__)
 # In production it is preferable to use a fixed and securely generated secret key instead of a random one each time the server restarts.
-# app.config['SECRET_KEY'] = os.urandom(24) # This will generate a random 24bit secret key
-app.config['SECRET_KEY'] = 'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SECRET_KEY'] = os.urandom(24) # This will generate a random 24bit secret key
+# app.config['SECRET_KEY'] = 'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K' # commented out for tests with postman
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://mysuperuser2:mysuperpass2@localhost:5432/sendit"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
@@ -34,8 +36,3 @@ api = Api(app)
 
 # Instantiate CORS
 CORS(app)
-
-# creates all database tables
-"""@app.before_first_request
-def create_tables():
-    db.create_all()"""
