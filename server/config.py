45f15ee3e4e6aacc842a3e5c789a4b86e4b06d25
Adam Mwaniki <adamndegwa@protonmail.com>
2024-08-26 04:18:29 +0300

    removes cors duplicate again

--- server/config.py
+++ server/config.py
@@ -35,8 +35,4 @@ CORS(app,
      methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
      expose_headers=['Set-Cookie'])
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    return response
+
